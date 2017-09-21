f77e66a49bdeb5ed86ccb059cbfabb9eea2534b8
root <yubq@yongjiasoft.com>
2017-09-21 21:23:05 +0800

    实例提交

---
+++ 源码/Club/Club/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Club
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "Club.Controllers" }
+
+            );
+        }
+    }
+}
