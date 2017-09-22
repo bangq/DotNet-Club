7f2979571df874904052e1d66a0d01191d2e70ed
root <yubq@yongjiasoft.com>
2017-09-22 09:00:50 +0800

    模版布局

---
+++ 源码/Club/Club/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Club.Areas.Admin.Controllers
+{
+    public class PostController : Controller
+    {
+        /// <summary>
+        /// 帖子列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
