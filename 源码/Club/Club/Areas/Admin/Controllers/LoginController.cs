ba40c0189dc3190ff90b0c419fe4c9e6505b81b9
root <yubq@yongjiasoft.com>
2017-09-25 09:00:32 +0800

    登录页面

---
+++ 源码/Club/Club/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Club.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
