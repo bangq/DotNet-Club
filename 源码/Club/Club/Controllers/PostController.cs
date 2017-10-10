cdcdf089a9edfcc6662d42d0d7aacbbc81f369d8
root <yubq@yongjiasoft.com>
2017-10-10 14:53:57 +0800

    富文本编辑器

---
+++ 源码/Club/Club/Controllers/PostController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Club.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
