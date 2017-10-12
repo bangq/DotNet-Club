1f09805af9483ac6bcf83098e10556cf8b44d306
root <yubq@yongjiasoft.com>
2017-10-12 14:56:49 +0800

    post 新增

--- 源码/Club/Club/Controllers/PostController.cs
+++ 源码/Club/Club/Controllers/PostController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace Club.Controllers
 {
-    public class PostController : Controller
+    public class PostController : BaseController
     {
         // GET: Post
         public ActionResult Index()
@@ -23,6 +23,49 @@ namespace Club.Controllers
                 ViewBag.Categorys = categorys;
             }
             return View();
+        }
+
+        [WebAuthFilter(IsNeedLogin = true)]
+        [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult Save()
+        {
+            var categoryId = Request["categoryId"].ToInt();
+            var title = Request["title"];
+            var content = Request["content"];
+            var loginUser = (User)Session["loginUser"];
+
+            if (categoryId == 0)
+            {
+                ShowMassage("帖子分类不能为空");
+                return RedirectToAction("New");
+            }
+
+            if (string.IsNullOrEmpty(title))
+            {
+                ShowMassage("帖子标题不能为空");
+                return RedirectToAction("New");
+            }
+            if (string.IsNullOrEmpty(content))
+            {
+                ShowMassage("帖子内容不能为空");
+                return RedirectToAction("New");
+            }
+
+            using (var db=new ClubEntities())
+            {
+                var post=new Post();
+                post.CategoryId = categoryId;
+                post.Title = title;
+                post.Details = content;
+                post.CreateTime=DateTime.Now;
+                post.UserId = loginUser.Id;
+                db.Post.Add(post);
+                db.SaveChanges();
+                ShowMassage("发布成功");
+                return Redirect("/");
+            }
+
         }
     }
 }
